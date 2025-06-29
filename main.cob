ec6909e6cac2677379a35455997d4cc8ed66f57f
Michael Steinmötzger <m.steinmoetzger@gmail.com>
2025-06-29 02:14:27 +0200

    because gradients are nice, especially when its created by a piece of software designed for some accountants to write paychecks

--- main.cob
+++ main.cob
@@ -1,12 +1,52 @@
 identification division.
 program-id. themistake.
+
+environment division.
+input-output section.
+file-control.
+       select out-file assign to "output.ppm"
+       organization is line sequential.
+
 data division.
+      
+      file section.
+      fd out-file.
+      01 out-rec pic x(400000).
+      
       working-storage section.
       01 ray-origin.
        05 origin-x pic s9(5)v9(5) value +0.
        05 origin-y pic s9(5)v9(5) value +0.                         
-       05 origin-z pic s9(5)v9(5) value +0.                                     
+       05 origin-z pic s9(5)v9(5) value +0.
+      01 width pic 9(3) value 200.
+      01 height pic 9(3) value 200.                                     
+      01 X pic 9(3).
+      01 Y pic 9(3).
+      01 Z pic 9(3).        
+      01 r pic 9(3).
+      01 g pic 9(3).
+      01 b pic 9(3).                           
 procedure division.
-       compute origin-x = origin-x + 1.0
-       display "Hello, World" origin-x
+       open output out-file
+       move "P3" to out-rec
+       write out-rec
+       move "200 200" to out-rec
+       write out-rec
+       move "255" to out-rec
+       write out-rec
+       perform varying y from 1 by 1 until y > height
+           perform varying x from 1 by 1 until x > width
+               compute r = (x * 255) / width
+               compute g = (y * 255) / height
+               compute b = 128
+               string r delimited by size
+                   " " delimited by size
+                   g delimited by size
+                   " " delimited by size
+                   b delimited by size
+               into out-rec
+               write out-rec
+           end-perform
+       end-perform
+       close out-file
        stop run.                                                        
